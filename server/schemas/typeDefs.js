4ba8cd3661841744764ecdfabcd22a96c1fb127d
mjzabriskie <99937908+mjzabriskie@users.noreply.github.com>
2022-08-05 22:02:18 -0600

    added mutations for thoughts and users

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -25,17 +25,23 @@ const typeDefs = gql`
     createdAt: String
     username: String
   }
-  type Reaction {
-    _id: ID
-    reactionBody: String
-    createAt: String
-    username: String
-  }
   type Query {
+    me: User
     users: [User]
     user(username: String!): User
     thoughts(username: String): [Thought]
     thought(_id: ID!): Thought
+  }
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addThought(thoughtText: String!): Thought
+    addReaction(thoughtId: ID!, reactionBody: String!): Thought
+    addFriend(friendId: ID!): User
+  }
+  type Auth {
+    token: ID!
+    user: User
   }
 `;
 
